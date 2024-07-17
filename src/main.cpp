2448a2a31f5568afdd4cd96064ad51f5075393bb
Oséias K. Rocha <orocha@ael.com.br>
2024-07-17 19:38:13 -0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+
+int main()
+{
+    std::cout << "Teste" << std::endl;
+    print();
+}
---
+++ src/test.h
@@ -0,0 +1,11 @@
+#ifndef TEST_H_
+#define TEST_H_
+
+#include <iostream>
+
+inline void print()
+{
+    std::cout << "Test" << std::endl;
+}
+
+#endif
