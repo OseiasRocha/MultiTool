c1057f7a168633c0d6bbbd45a341e695e51a67b7
Oséias K. Rocha <orocha@ael.com.br>
2024-07-25 10:14:07 -0300

    Adding first tests with imgui

--- MultiTool/main.cpp
+++ MultiTool/main.cpp
@@ -11,6 +11,7 @@
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
+#include <iostream>
 #include <SDL.h>
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <SDL_opengles2.h>
@@ -83,7 +84,6 @@ int main(int, char**)
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
     //io.ConfigViewportsNoAutoMerge = true;
@@ -127,6 +127,11 @@ int main(int, char**)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    char buf[128];
+    float f;
+
+    bool ftpClientStarted = false;
+    bool windowOpen = true;
     // Main loop
     bool done = false;
 #ifdef __EMSCRIPTEN__
@@ -135,7 +140,7 @@ int main(int, char**)
     io.IniFilename = nullptr;
     EMSCRIPTEN_MAINLOOP_BEGIN
 #else
-    while (!done)
+    while (!done && windowOpen)
 #endif
     {
         // Poll and handle events (inputs, window resize, etc.)
@@ -157,43 +162,67 @@ int main(int, char**)
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
+        bool open = false;
+        bool save = false;
+        bool saveas = false;
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        /*ImGui::SetNextWindowSize(ImGui::GetContentRegionMax());*/
+        ImGui::Begin("Multitool", &windowOpen, ImGuiWindowFlags_NoSavedSettings);
+        if (ImGui::BeginMainMenuBar())
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            if (ImGui::BeginMenu("File", true))
+            {
+                if (ImGui::MenuItem("Open", nullptr, nullptr))
+                {
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
+                }
+                ImGui::MenuItem("Save...", nullptr, nullptr);
+                ImGui::MenuItem("Save as", nullptr, nullptr);
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
+        ImGui::BeginTabBar("Tabs");
+        if (ImGui::BeginTabItem("FTP Server"))
         {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
+            // startar serviço de FTP
+            ImGui::Text("FTP Server");
+            ImGui::EndTabItem();
         }
+        else
+        {
+            // matar seviço de FTP
+        }
+
+
+        if (ImGui::BeginTabItem("FTP Client"))
+        {
+            // client
+            ImGui::Text("FTP Client");
+            ImGui::EndTabItem();
+            ftpClientStarted = true;
+        }
+        else
+        {
+            if (ftpClientStarted) {
+                // fechar conexão com o servidor FTP
+                ImGui::OpenPopup("clearingFTPClient");
+                if (ImGui::BeginPopupModal("clearingFTPClient"))
+                {
+                    ImGui::Text("Limpando client FTP");
+                    if (ImGui::Button("close"))
+                    {
+                        ftpClientStarted = false;
+                        ImGui::CloseCurrentPopup();
+                    }
+                    ImGui::EndPopup();
+                }
+            }
+        }
+
+        ImGui::EndTabBar();
+        ImGui::End();
 
         // Rendering
         ImGui::Render();
